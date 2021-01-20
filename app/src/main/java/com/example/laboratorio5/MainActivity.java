ea0f0c4353177aa7213de664d8818e4e2713d9b4
BrandonRodriguezC <410brandonrodriguez@gmail.com>
2021-01-20 10:02:50 -0500

    Intent

--- app/src/main/java/com/example/laboratorio5/MainActivity.java
+++ app/src/main/java/com/example/laboratorio5/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.laboratorio5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button registro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        registro = (Button) findViewById(R.id.log_registro);
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nuevaActividad = new Intent(MainActivity.this, Register.class);
+                startActivity(nuevaActividad);
+            }
+        });
     }
 }
